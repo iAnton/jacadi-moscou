9a83e34c1cf347dc76e94d19a7ebe5bcce534acd
Silentel <dnbsilent@gmail.com>
2020-06-15 13:44:40 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,165 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link @click="$router.push('/')">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Главная</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-new-box</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Новые поступления</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-percent</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Распродажа</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>О нас</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="light-blue accent-2" class="white--text">
+      <v-app-bar-nav-icon
+        class="white--text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="mr-2">Название компании</v-toolbar-title>
+      <div>
+        <span>Логотип</span>
+        <!-- <v-img
+          src="https://media.istockphoto.com/vectors/baby-shop-logo-detailed-illustration-vector-id950062114?k=6&m=950062114&s=170667a&w=0&h=m-jDaC_ns5VMyyGHiyUf9j7f9QztbrTkOo6Vs3i0vDE="
+          max-height="100"
+          contain
+        ></v-img> -->
+      </div>
+      <v-text-field
+        class="mx-4"
+        flat
+        hide-details
+        prepend-inner-icon="search"
+        solo
+      ></v-text-field>
+      <nuxt-link class="d-flex flex-column mx-2" to="/">
+        <v-icon>mdi-map-marker</v-icon>
+        <span>Магазины</span>
+      </nuxt-link>
+      <nuxt-link class="d-flex flex-column mx-2" to="/">
+        <v-icon>mdi-account</v-icon>
+        <span>Аккаунт</span>
+      </nuxt-link>
+      <nuxt-link class="d-flex flex-column mx-2" to="/">
+        <v-icon>mdi-heart</v-icon>
+        <span>Избранное</span>
+      </nuxt-link>
+      <nuxt-link class="d-flex flex-column mx-2" to="/">
+        <v-icon>mdi-cart</v-icon>
+        <span>Корзина</span>
+      </nuxt-link>
+    </v-app-bar>
+
+    <v-main>
+      <v-breadcrumbs :items="items">
+        <template v-slot:divider>
+          <v-icon>mdi-chevron-right</v-icon>
+        </template>
+      </v-breadcrumbs>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app absolute padless>
+      <v-card
+        width="100%"
+        flat
+        tile
+        class="light-blue accent-2 lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-spacer></v-spacer>
+
+        <v-card-text class="d-flex justify-space-around white--text pt-0">
+          <nuxt-link to="/">О нас</nuxt-link>
+          <nuxt-link to="/">Контакты</nuxt-link>
+          <nuxt-link to="/">Политика конфиденциальности</nuxt-link>
+          <nuxt-link to="/">Условия использования</nuxt-link>
+          <nuxt-link to="/">Часто задаваемые вопросы</nuxt-link>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} —
+          <strong>Интернет-магазин детских вещей</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+    <v-footer>
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+      items: [
+        {
+          text: 'Главная',
+          disabled: false,
+          href: '/'
+        },
+        {
+          text: 'Одежда',
+          disabled: false,
+          href: '/'
+        },
+        {
+          text: 'Футболки',
+          disabled: true,
+          href: '/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,240 @@
+<template>
+  <div>
+    <v-row column>
+      <v-col cols="12" sm="6" md="8">
+        <v-card>
+          <v-card-title class="headline">
+            Футболка
+          </v-card-title>
+          <v-card-text>
+            <v-carousel
+              cycle
+              height="400"
+              hide-delimiter-background
+              show-arrows-on-hover
+            >
+              <v-carousel-item v-for="(slide, i) in slides" :key="i">
+                <v-sheet :color="colors[i]" height="100%">
+                  <v-row class="fill-height" align="center" justify="center">
+                    <div class="display-3">{{ slide }}</div>
+                  </v-row>
+                </v-sheet>
+              </v-carousel-item>
+            </v-carousel>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="light-blue accent-2" class="white--text" nuxt to="/">
+              Купить
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-card>
+          <v-card-title class="headline">
+            Футболка
+          </v-card-title>
+          <v-card-text>
+            <v-expansion-panels accordion class="mb-2">
+              <v-expansion-panel>
+                <v-expansion-panel-header>Описание</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+              <v-expansion-panel>
+                <v-expansion-panel-header>Состав</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+            <div class="mb-2">
+              <span>Цвет:</span>
+              <v-item-group class="mt-2" mandatory>
+                <v-item
+                  v-for="color in colors"
+                  :key="color"
+                  v-slot:default="{ active, toggle }"
+                >
+                  <v-chip
+                    :active-class="color"
+                    :input-value="active"
+                    @click="toggle"
+                  >
+                    {{ color }}
+                  </v-chip>
+                </v-item>
+              </v-item-group>
+            </div>
+            <div class="mb-4">
+              <span>Размер:</span>
+              <v-item-group mandatory class="mt-2">
+                <v-item
+                  v-for="size in sizes"
+                  :key="size"
+                  v-slot:default="{ active, toggle }"
+                >
+                  <v-chip
+                    active-class="light-blue accent-2"
+                    :input-value="active"
+                    @click="toggle"
+                  >
+                    {{ size }}
+                  </v-chip>
+                </v-item>
+              </v-item-group>
+            </div>
+            <div class="mb-2">Цена: <b>100 руб.</b></div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="light-blue accent-2" class="white--text">
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn color="light-blue accent-2" class="white--text" nuxt to="/">
+              Добавить в корзину
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="d-flex flex-column mb-4">
+      <v-col class="text-center"><h3>Вам также может понравится</h3></v-col>
+      <v-col class="d-flex justify-space-between">
+        <v-card class="mx-auto" max-width="300">
+          <v-img
+            src="https://www.hallmark.com/dw/image/v2/AALB_PRD/on/demandware.static/-/Sites-hallmark-master/default/dw53dfd4ab/images/finished-goods/Hi-Im-New-Here-Infant-Bodysuit_1SHT2036_01.jpg?sw=512&sh=512&sm=fit"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Название товара
+          </v-card-title>
+
+          <v-card-subtitle>
+            Цена: 100 руб
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="gray" text>
+              Посмотреть
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="mx-auto" max-width="300">
+          <v-img
+            src="https://yolostore.nz/wp-content/uploads/2017/09/vintage-baby-clothes-600x600.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Название товара
+          </v-card-title>
+
+          <v-card-subtitle>
+            Цена: 100 руб
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="gray" text>
+              Посмотреть
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-card class="mx-auto" max-width="300">
+          <v-img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT1vBvWodFO381qFugKl8ctyQ5gyfIpBzEjAunV4NZpoXaHBXox&usqp=CAU"
+            height="200px"
+          ></v-img>
+
+          <v-card-title>
+            Название товара
+          </v-card-title>
+
+          <v-card-subtitle>
+            Цена: 100 руб
+          </v-card-subtitle>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="gray" text>
+              Посмотреть
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row>
+      <v-col class="d-flex justify-space-around">
+        <div class="d-flex align-center">
+          <v-icon class="pa-4">mdi-phone</v-icon>
+          <div class="d-flex flex-column">
+            <span>Поддержка</span>
+            <nuxt-link to="/">Подробнее</nuxt-link>
+          </div>
+        </div>
+        <div class="d-flex align-center">
+          <v-icon class="pa-4">mdi-sync</v-icon>
+          <div class="d-flex flex-column">
+            <span>Бесплатный возврат товара</span>
+            <nuxt-link to="/">Подробнее</nuxt-link>
+          </div>
+        </div>
+        <div class="d-flex align-center">
+          <v-icon class="pa-4">mdi-train-car</v-icon>
+          <div class="d-flex flex-column">
+            <span>Бесплатная доставка</span>
+            <nuxt-link to="/">Подробнее</nuxt-link>
+          </div>
+        </div>
+        <div class="d-flex align-center">
+          <v-icon class="pa-4">mdi-lock</v-icon>
+          <div class="d-flex flex-column">
+            <span>Защищенные платежи</span>
+            <nuxt-link to="/">Подробнее</nuxt-link>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row>
+      <v-col class="d-flex justify-space-around">
+        <div class="d-flex flex-column align-center">
+          <h3 class="mb-2">Подписаться на рассылку</h3>
+          <v-text-field label="E-mail" outlined></v-text-field>
+          <v-btn color="light-blue accent-2">Подписаться</v-btn>
+        </div>
+        <div class="d-flex flex-column align-center">
+          <h3 class="mb-2">Наши магазины</h3>
+          <span class="grey--text mb-4">
+            Найти ближайшие магазины в вашем городе
+          </span>
+          <v-btn color="light-blue accent-2">Найти</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: ['red', 'blue', 'green', 'yellow', 'pink'],
+      sizes: ['xs', 's', 'm', 'l', 'xl'],
+      slides: ['1', '2', '3', '4', '5']
+    }
+  }
+}
+</script>

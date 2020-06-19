a39558f4232a81f2d1492008b10e9eafd34c5b91
Anton Ignatov <anton@ignatov.ae>
2020-06-19 21:30:21 +0300

    Deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,9 @@ export default {
   /*
    ** Headers of the page
    */
-  // router: {
-  //   base: '/online-shop/'
-  // },
+  router: {
+    base: '/jacadi-moscou/'
+  },
   head: {
     // titleTemplate: '%s - ' + 'Jacadi детская одежда',
     titleTemplate: 'Jacadi детская одежда',
